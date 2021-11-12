af3890b669d28573dc896a7b9b4178d59043aa78
coynek <91563299+coynek@users.noreply.github.com>
2021-11-12 17:05:01 +0000

    final version.

---
+++ files.py
@@ -0,0 +1,12 @@
+import os
+
+score = input("What's the score?")
+if int(score) > int(file.read):
+
+ # read the current file
+# file = open("highscore.txt", "r")
+score = file.read()
+print(score)
+if changing_file:
+os.remove
+file.close()
--- keyboardturtle.py
+++ keyboardturtle.py
@@ -22,9 +22,9 @@ class KeyboardTurtle(Turtle):
 
     #set turtle starting states
     self.shape("turtle")
-    self.color("green")
+    self.color("light blue")
     self.penup()
-
+    self.x_speed = 5 
     # Sets up keyboard command examples
     self.window.onkey(self.go_right, self.turn_right)
     self.window.onkey(self.go_forward, self.straight)
@@ -33,25 +33,21 @@ class KeyboardTurtle(Turtle):
 
 
     #sets up controlling variables (y not implemented)
-    self.movement_speed = 5
-    self.turn_speed = 45
+    self.movement_speed = 10
+    self.turn_speed = 10
     self.collision_distance = 20
 
   # Movement Methods
   def go_forward(self):
     self.forward(self.movement_speed)
-    if self.check_collision(self.other_player):
-      print("crash")
-      quit()
+    
       
   def go_right(self):
     self.right(self.turn_speed)
 
   def go_back(self):
     self.forward(-self.movement_speed)
-    if self.check_collision(self.other_player):
-      print ("crash")
-      quit()
+    
 
 
   def go_left(self):
@@ -64,17 +60,7 @@ class KeyboardTurtle(Turtle):
   # it doesn't NEED to.  It can check against any
   # other turtle object
 
-  def check_collision(self, obj_to_check):
-    distance_x = obj_to_check.xcor() - self.xcor()
-    distance_x = abs(distance_x)
-
-    distance_y = obj_to_check.ycor() - self.ycor()
-    distance_y = abs(distance_y)
-
-    if distance_x < self.collision_distance and distance_y < self.collision_distance:
-      return True
-    else:
-      return False
+  
 
 
     # TODO: finish setting up the inputs (left and down)
